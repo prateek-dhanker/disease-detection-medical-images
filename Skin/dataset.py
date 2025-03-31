c0c2a370d78a21149d1731c860955c02c3fc5866
Gaurav959987 <mamtagk465@gmailcon>
2025-03-31 14:04:43 +0530

    Deleted Skin folder from repository

--- Skin/dataset.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("pritpal2873/multiple-skin-disease-detection-and-classification")
-
-print("Path to dataset files:", path)
